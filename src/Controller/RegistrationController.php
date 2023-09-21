97ebd1422c664e803cdae90eaa149c2ff0d3b749
KaidoOwls <seydina.diallo60@gmail.com>
2023-09-21 16:51:57 +0200

    modif

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
 use App\Security\UsersAuthenticator;
+use App\Service\JWTService;
 use App\Service\SendMailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,7 +19,7 @@ class RegistrationController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator,
-    UsersAuthenticator $authenticator, EntityManagerInterface $entityManager, SendMailService $mail ): Response
+    UsersAuthenticator $authenticator, EntityManagerInterface $entityManager, SendMailService $mail, JWTService $jwt ): Response
     {
         $user = new Users();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -37,16 +38,29 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
+            //on génère le jwt de l'utilisateur
+            //on crée le header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            // on crée le payload 
+            $payload= [
+                'user_id' => $user->getId()
+            ];
+
+            //on génère le token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+
             // on envoie un mail
             $mail->send(
                 'no-reply@monsite.net',
                 $user->getEmail(),
                 'Activation de votre compte sur le site The District',
                 'register',
-                [
-                compact('user')
-                ]
-            
+                compact('user', 'token')
             );
 
 
@@ -61,4 +75,12 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
-}
+
+    #[Route('/verif/{token}', name: 'verify_user')]
+        public function verifyUser($token, JWTService $jwt): Response
+        {
+            dd($jwt->isExpired($token));
+
+        }
+        
+    }
