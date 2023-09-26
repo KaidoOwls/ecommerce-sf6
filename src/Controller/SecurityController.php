c277479a2df184f2bfbe45797dea483c378066db
KaidoOwls <seydina.diallo60@gmail.com>
2023-09-26 16:10:35 +0200

    modif

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/connexion', name: 'app_login')]
+    #[Route('/connexion', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -27,9 +27,12 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/deconnexion', name: 'app_logout')]
+    #[Route('/deconnexion', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route('/oubli-pass', name:'forgotten_password')]
+    public function
 }
