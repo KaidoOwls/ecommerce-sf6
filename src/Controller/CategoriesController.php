50e162a6513518e1f593fca074c29cbad854ff68
KaidoOwls <seydina.diallo60@gmail.com>
2023-10-03 16:50:48 +0200

    pagination ajout

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -8,20 +8,22 @@ use App\Repository\PlatRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/categories', name: 'categories_')]
 class CategoriesController extends AbstractController
 {
 // Accolade permet de récup l'id avec slug
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category, PlatRepository $platRepository): Response
+    public function list(Categories $category, PlatRepository $platRepository, Request $request): Response
     
     {   
-        // on va chercher la list des plats de la catégorie
-        $plat = $platRepository->findPlatPaginated(1, $category->getSlug(), 2);
+        // on va chercher le numéro de page dans l'url 
+        $page = $request->query->getInt('page', 1);
 
-        dd($plat);
+
+        // on va chercher la list des plats de la catégorie
+        $plat = $platRepository->findPlatPaginated($page, $category->getSlug(), 2);
 
         return $this->render('categories/list.html.twig', compact('category','plat'));
 
