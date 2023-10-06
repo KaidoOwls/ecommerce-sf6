5f3e6711c36f1b99665f94240e9e1a2291885c3b
KaidoOwls <seydina.diallo60@gmail.com>
2023-10-06 10:19:47 +0200

    réglage images

--- src/Controller/Admin/PlatsController.php
+++ src/Controller/Admin/PlatsController.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Plat;
 use App\Form\PlatsFormType;
-use APP\Service\PictureService\PictureService;
+use App\Service\PictureService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/plats', name:'admin_plats_')]
 class PlatsController extends AbstractController
--- src/Service/PictureService.php
+++ src/Service/PictureService.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace APP\Service\PictureService;
+namespace App\Service;
 
+use Exception;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -16,10 +17,10 @@ class PictureService
 
     public function add(UploadedFile $picture, ?string $folder = '', ?int $width = 250, ?int $height = 250)
     {
-        // On donne un nouveau nom à l'image 
+        // On donne un nouveau nom à l'image
         $fichier = md5(uniqid(rand(), true)) . '.webp';
 
-        // on va récupérer les infos de l'image 
+        // On récupère les infos de l'image
         $picture_infos = getimagesize($picture);
 
         if($picture_infos === false){
@@ -32,54 +33,52 @@ class PictureService
                 $picture_source = imagecreatefrompng($picture);
                 break;
             case 'image/jpeg':
-                $picturesource = imagecreatefromjpeg($picture);
+                $picture_source = imagecreatefromjpeg($picture);
                 break;
             case 'image/webp':
                 $picture_source = imagecreatefromwebp($picture);
                 break;
             default:
-            throw new Exception('Format d\'image incorrect');
+                throw new Exception('Format d\'image incorrect');
         }
 
-        // on recadre l'image
-        // on récupère les dimensions
+        // On recadre l'image
+        // On récupère les dimensions
         $imageWidth = $picture_infos[0];
         $imageHeight = $picture_infos[1];
 
         // On vérifie l'orientation de l'image
         switch ($imageWidth <=> $imageHeight){
-            case -1: //portrait 
-                $squareSize = $imageWidth;
-                $src_x = 0;
-                $src_y = 0;
-                break;
-
-            case -0: //carré 
+            case -1: // portrait
                 $squareSize = $imageWidth;
                 $src_x = 0;
                 $src_y = ($imageHeight - $squareSize) / 2;
                 break;
-
-            case 1: //paysage 
+            case 0: // carré
+                $squareSize = $imageWidth;
+                $src_x = 0;
+                $src_y = 0;
+                break;
+            case 1: // paysage
                 $squareSize = $imageHeight;
                 $src_x = ($imageWidth - $squareSize) / 2;
                 $src_y = 0;
                 break;
         }
 
-        // on crée une nouvelle image "vierge"
+        // On crée une nouvelle image "vierge"
         $resized_picture = imagecreatetruecolor($width, $height);
 
         imagecopyresampled($resized_picture, $picture_source, 0, 0, $src_x, $src_y, $width, $height, $squareSize, $squareSize);
 
         $path = $this->params->get('images_directory') . $folder;
 
-        // on crée le dossier de destination s'il n'existe pas 
+        // On crée le dossier de destination s'il n'existe pas
         if(!file_exists($path . '/mini/')){
             mkdir($path . '/mini/', 0755, true);
         }
 
-        // on stocke l'image recadrée 
+        // On stocke l'image recadrée
         imagewebp($resized_picture, $path . '/mini/' . $width . 'x' . $height . '-' . $fichier);
 
         $picture->move($path . '/', $fichier);
@@ -101,8 +100,9 @@ class PictureService
             }
 
             $original = $path . '/' . $fichier;
+
             if(file_exists($original)){
-                unlink($mini);
+                unlink($original);
                 $success = true;
             }
             return $success;
